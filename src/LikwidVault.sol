fa261406211161352b9e78df2806d26b9d2be484
finezoo <fighter2038@gmail.com>
2025-07-07 11:11:06 +0800

    modify: solmate remapping

--- src/LikwidVault.sol
+++ src/LikwidVault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {Owned} from "solmate/auth/Owned.sol";
+import {Owned} from "solmate/src/auth/Owned.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import {Currency, CurrencyLibrary} from "./types/Currency.sol";
